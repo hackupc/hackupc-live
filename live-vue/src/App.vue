3d56d8bedec2ed9f825b41fe5453e23b5b77f2bb
bernatixer <bernat.torres.bellido@gmail.com>
2020-03-15 23:18:16 +0100

    Splitting FAQ, Rules and Navbar

--- live-vue/src/App.vue
+++ live-vue/src/App.vue
@@ -1,32 +1,118 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <!--header for <720px-->
+    <header id="header-small" class="show-when-small">
+      <div class="bar">
+        <div id="open-aside-btn">
+          <span>&#9776;</span>
+        </div>
+        <div class="title-container">
+          <h1 id="title">Live</h1>
+        </div>
+      </div>
+    </header>
+    <!--Aside menu for small screens-->
+    <aside id="aside-small-menu" class="show-when-small closed hidden">
+      <div id="close-aside-btn">
+        <div>x</div>
+      </div>
+      <nav>
+        <ul>
+          <li data-bind-view="dayof">
+            <a href="#/dayof">Home</a>
+          </li>
+          <li data-bind-view="live">
+            <a href="#/live">Live</a>
+          </li>
+          <li data-bind-view="schedule">
+            <a href="#/schedule">Schedule</a>
+          </li>
+          <li data-bind-view="map">
+            <a href="#/map">Map</a>
+          </li>
+          <!-- <li data-bind-view="mentors">-->
+          <li>
+            <!-- <a href="#/mentors">Mentors</a> -->
+            <a href="https://mentors.hackupc.com/" target="_blank">Mentors</a>
+          </li>
+          <li data-bind-view="streaming">
+            <a href="#/streaming">Streaming</a>
+          </li>
+          <li data-bind-view="challenges">
+            <a href="#/challenges">Challenges</a>
+          </li>
+          <!-- <li data-bind-view="guides">-->
+          <li>
+            <!-- <a href="#/guides">Guides</a> -->
+            <a href="https://guides.hackupc.com/guide/hacker" target="_blank">Guides</a>
+          </li>
+          <li data-bind-view="rules">
+            <a href="#/rules">Rules</a>
+          </li>
+          <li data-bind-view="faq">
+            <a href="#/faq">FAQ</a>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+    <!--header for >720px-->
+    <header id="header-nav-bar" class="hide-when-small">
+      <nav>
+        <ul>
+          <li data-bind-view="dayof">
+            <a href="#/dayof">Home</a>
+          </li>
+          <li data-bind-view="live">
+            <a href="#/live">Live</a>
+          </li>
+          <li data-bind-view="schedule">
+            <a href="#/schedule">Schedule</a>
+          </li>
+          <li data-bind-view="map">
+            <a href="#/map">Map</a>
+          </li>
+          <!-- <li data-bind-view="mentors"> -->
+          <li>
+            <a href="https://mentors.hackupc.com/" target="_blank">Mentors</a>
+            <!-- <a href="#/mentors">Mentors</a> -->
+          </li>
+          <li id="countdown-li">
+            <div class="countdown">
+              <div id="countdown-time" class="countdown-time">
+                <span class="hours">36</span>:<span class="minutes">00</span><span class="seconds">00</span>
+              </div>
+              <div class="countdown-bg">
+                <img src="/assets/img/live/hackupc-logo.svg" alt="HackUPC logo">
+              </div>
+            </div>
+          </li>
+          <li data-bind-view="streaming">
+            <a href="#/streaming">Streaming</a>
+          </li>
+          <li data-bind-view="challenges">
+            <a href="#/challenges">Challenges</a>
+          </li>
+          <!-- <li data-bind-view="guides">-->
+          <li>
+            <!-- <a href="#/guides">Guides</a> -->
+            <a href="https://guides.hackupc.com/guide/hacker" target="_blank">Guides</a>
+          </li>
+          <li data-bind-view="rules">
+            <!-- <a href="/rules">Rules</a> -->
+            <router-link to="/rules#/rules">Rules</router-link>
+          </li>
+          <li data-bind-view="faq">
+            <!-- <a href="#/faq">FAQ</a> -->
+            <router-link to="/faq#/faq">FAQ</router-link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <router-view/>
+    </main>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
