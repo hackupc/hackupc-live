be736bce913254bbf31fcc8fbb5b1d1e45e5f5c5
bernatixer <bernat.torres.bellido@gmail.com>
2020-03-16 20:31:47 +0100

    Adding Vuex store

---
+++ live-vue/src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    totalTvCount: 10,
+  },
+  getters: {
+    // Here we will create a getter
+  },
+  mutations: {
+    // Here we will create Jenny
+  },
+  actions: {
+    // Here we will create Larry
+  },
+});
