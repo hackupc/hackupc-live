8210c99ab8cb001244fed25f94a0ffe66f647048
mauriciabad <hello@mauriciabad.com>
2020-04-21 17:19:37 +0200

    Revert "Revert "Disable live""
    This reverts commit 1145184180c7b0be394f3c5c636e4a2638780753.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div v-if="!disabled">
     <Notification/>
     <!--header for <720px-->
     <header id="header-small" :class="[this.isFullscreen ? 'hidden' : '', 'show-when-small']">
@@ -107,6 +107,7 @@
 </template>
 
 <script>
+import Config from '@/config';
 import Countdown from '@/components/Countdown.vue';
 import Notification from '@/components/Notification.vue';
 
@@ -117,6 +118,7 @@ export default {
       isFullscreen: false,
       asideMenuClosed: true,
       asideMenuHidden: true,
+      disabled: Config.disabled,
     };
   },
   methods: {
@@ -192,5 +194,17 @@ export default {
 }
 .fade-enter, .fade-leave-active {
   opacity: 0
+}
+.disabled-msg{
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2.5rem;
+  padding: 1rem;
+  box-sizing: border-box;
+  text-align: center;
+  font-weight: bolder;
+  text-shadow: 0 0 1rem rgb(255, 255, 255);
 }
 </style>
