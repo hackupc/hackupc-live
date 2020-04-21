1145184180c7b0be394f3c5c636e4a2638780753
mauriciabad <hello@mauriciabad.com>
2020-04-21 16:43:59 +0200

    Revert "Disable live"
    This reverts commit c6ec701ebcc2e096716c76c7072d602381cea692.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="!disabled">
+  <div>
     <Notification/>
     <!--header for <720px-->
     <header id="header-small" :class="[this.isFullscreen ? 'hidden' : '', 'show-when-small']">
@@ -100,14 +100,9 @@
       </transition>
     </main>
   </div>
-  <div v-else class="disabled-msg">
-    Sorry, live is not aviable yet. <br>
-    Come back later.
-  </div>
 </template>
 
 <script>
-import Config from '@/config';
 import Countdown from '@/components/Countdown.vue';
 import Notification from '@/components/Notification.vue';
 
@@ -118,7 +113,6 @@ export default {
       isFullscreen: false,
       asideMenuClosed: true,
       asideMenuHidden: true,
-      disabled: Config.disabled,
     };
   },
   methods: {
@@ -194,17 +188,5 @@ export default {
 }
 .fade-enter, .fade-leave-active {
   opacity: 0
-}
-.disabled-msg{
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 2.5rem;
-  padding: 1rem;
-  box-sizing: border-box;
-  text-align: center;
-  font-weight: bolder;
-  text-shadow: 0 0 1rem rgb(255, 255, 255);
 }
 </style>
