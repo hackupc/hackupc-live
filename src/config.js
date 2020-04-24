f57e1c93420444b4c6927c177f28f2053d8410cd
Maurici Abad Gutierrez <hello@mauriciabad.com>
2020-04-24 20:24:40 +0200

    Enable live

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ import schedule from '../public/data/schedule.json';
 export default {
   // If you don't want to test, just comment the fakeTime field
   fakeTime: new Date('2019-10-11 20:59:55'), // year, month, day, time
-  disabled: true,
+  disabled: false,
   notificationTitle: 'HackUPC 2020',
   notificationIcon: 'favicon.ico',
   baseTimeOffset: -120 * 60,
