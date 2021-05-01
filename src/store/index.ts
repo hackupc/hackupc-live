abf4076a728f04e27aa158311f1b770ca93ff65c
Maurici Abad <maurici.abad@glovoapp.com>
2021-05-01 21:20:10 +0200

    Replace all tabs with spaces

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,17 +6,17 @@ import Config from '@/config';
 Vue.use(Vuex);
 
 function getDateTimestamp(date: string): number {
-	var dateFormat = /([0-3]?\d)\W([0-1]?\d)\W(\d{4})(\W([0-2]?\d)\W([0-5]?\d)\W?([0-5]?\d)?)?/;
-	var result = date.match(dateFormat)?.map(r => Number(r));
-	if(!result) throw new Error("Wrong date format");
-	return Date.UTC(
-		result[3],
-		result[2] - 1,
-		result[1],
-		result[5] || 0,
-		result[6] || 0,
-		result[7] || 0
-	) / 1000;
+  var dateFormat = /([0-3]?\d)\W([0-1]?\d)\W(\d{4})(\W([0-2]?\d)\W([0-5]?\d)\W?([0-5]?\d)?)?/;
+  var result = date.match(dateFormat)?.map(r => Number(r));
+  if(!result) throw new Error("Wrong date format");
+  return Date.UTC(
+    result[3],
+    result[2] - 1,
+    result[1],
+    result[5] || 0,
+    result[6] || 0,
+    result[7] || 0
+  ) / 1000;
 }
 
 function getHourTimestamp(hour: string): number {
@@ -25,17 +25,17 @@ function getHourTimestamp(hour: string): number {
 }
 
 function dateStringToSeconds(date: string): number {
-	var dateFormat = /([0-3]?\d)\W([0-1]?\d)\W(\d{4})(\W([0-2]?\d)\W([0-5]?\d)\W?([0-5]?\d)?)?/;
-	var result = date.match(dateFormat)?.map(r => Number(r));
-	if (!result) throw new Error("Wrong date format");
-	return Date.UTC(
-		result[3],
-		result[2] - 1,
-		result[1],
-		result[5] || 0,
-		result[6] || 0,
-		result[7] || 0
-	) / 1000;
+  var dateFormat = /([0-3]?\d)\W([0-1]?\d)\W(\d{4})(\W([0-2]?\d)\W([0-5]?\d)\W?([0-5]?\d)?)?/;
+  var result = date.match(dateFormat)?.map(r => Number(r));
+  if (!result) throw new Error("Wrong date format");
+  return Date.UTC(
+    result[3],
+    result[2] - 1,
+    result[1],
+    result[5] || 0,
+    result[6] || 0,
+    result[7] || 0
+  ) / 1000;
 }
 
 export default new Vuex.Store({
@@ -89,36 +89,36 @@ export default new Vuex.Store({
         commit('updateSubscribed', subscribed)
       },
       getSchedule({ commit }, cb) {
-         fetch('/data/schedule.json?date=' + Date.now())
-					.then((response) => response.json())
-					.then((data) => {
-							for (let i = 0; i < data.days.length; ++i) {
-								const startDateTimestamp = getDateTimestamp(data.days[i].date) + Config.baseTimeOffset;
-								data.days[i]["startTmsp"] = startDateTimestamp;
-								data.days[i]["endTmsp"] = startDateTimestamp + 24 * 60 * 60;
-								for (let j = 0; j < data.days[i].events.length; ++j) {
-										const startHourTimestamp = startDateTimestamp + getHourTimestamp(data.days[i].events[j].startHour);
-										data.days[i].events[j]["startTmsp"] = startHourTimestamp;
-										if (!data.days[i].events[j].endHour) {
-											data.days[i].events[j]["endTmsp"] = startHourTimestamp;
-										} else {
-											data.days[i].events[j]["endTmsp"] = startDateTimestamp + getHourTimestamp(data.days[i].events[j].endHour)
-										}
-										const event = data.days[i].events[j];
-										data.days[i].events[j].title = (event.talk ? '[Talk] ' : '') + (event.author ? (event.author + ': ') : '') + (event.title || '')
-								}
-							}
-							if ('version' in this.state.schedule && this.state.schedule.version !== data.version) {
-								console.log('Schedule updated with message: ' + data.message);
-								cb(data.message);
-							} else {
-								console.log('Schedule up to date');
-							}
-							commit("updateSchedule", data);
-					})
-					.catch((error) => {
-							console.log(error);
-					});
+        fetch('/data/schedule.json?date=' + Date.now())
+          .then((response) => response.json())
+          .then((data) => {
+            for (let i = 0; i < data.days.length; ++i) {
+              const startDateTimestamp = getDateTimestamp(data.days[i].date) + Config.baseTimeOffset;
+              data.days[i]["startTmsp"] = startDateTimestamp;
+              data.days[i]["endTmsp"] = startDateTimestamp + 24 * 60 * 60;
+              for (let j = 0; j < data.days[i].events.length; ++j) {
+                  const startHourTimestamp = startDateTimestamp + getHourTimestamp(data.days[i].events[j].startHour);
+                  data.days[i].events[j]["startTmsp"] = startHourTimestamp;
+                  if (!data.days[i].events[j].endHour) {
+                    data.days[i].events[j]["endTmsp"] = startHourTimestamp;
+                  } else {
+                    data.days[i].events[j]["endTmsp"] = startDateTimestamp + getHourTimestamp(data.days[i].events[j].endHour)
+                  }
+                  const event = data.days[i].events[j];
+                  data.days[i].events[j].title = (event.talk ? '[Talk] ' : '') + (event.author ? (event.author + ': ') : '') + (event.title || '')
+              }
+            }
+            if ('version' in this.state.schedule && this.state.schedule.version !== data.version) {
+              console.log('Schedule updated with message: ' + data.message);
+              cb(data.message);
+            } else {
+              console.log('Schedule up to date');
+            }
+            commit("updateSchedule", data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
      },
    },
 });
