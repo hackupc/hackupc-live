034b91ccd03514d19e409d49e0457d46b7904139
Maurici Abad <maurici.abad@glovoapp.com>
2021-05-05 22:31:04 +0200

    Remove unused countdown

--- src/views/Live.vue
+++ src/views/Live.vue
@@ -1,12 +1,6 @@
 <template>
   <div id="live" class="under-header">
     <div class="container-live">
-      <div
-        class="countdown-time countdown-time__mobile centered show-when-small"
-      >
-        <span class="hours">36</span>:<span class="minutes">00</span
-        ><span class="seconds">00</span>
-      </div>
       <div class="events-fancy">
         <ul>
           <template v-for="event in events">
