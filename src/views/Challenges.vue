b65283302040cdc83f2fe79c298f8a8cc323a3c3
David Dionis Soler <daviddionissoler@gmail.com>
2021-05-07 18:00:32 +0200

    Create Panel component

--- src/views/Challenges.vue
+++ src/views/Challenges.vue
@@ -1,61 +1,7 @@
 <template>
   <div id="schedule" class="under-header padding-bottom">
     <div class="container">
-      <div class="panel">
-        <h1>
-          Looking for similar patients: the AI Doctor House conquers severe
-          COVID-19!
-        </h1>
-        <div class="text-centered">
-          <p class="icon"><i class="fab fa-discord" aria-hidden="true"></i></p>
-          <span>similar-patients</span>
-        </div>
-        <div>
-          <h3>
-            <a
-              href="https://doi.org/10.5281/zenodo.4314709"
-              target="_blank"
-              class="external-link"
-              >Input data</a
-            >
-          </h3>
-          <h3>
-            <a
-              href="data/AIDoctor_info.pdf"
-              target="_blank"
-              class="external-link"
-              >More information!</a
-            >
-          </h3>
-          <h3>
-            <a
-              href="https://youtube.com/playlist?list=PL5uSCzf1azhBeVCHyswazImBNpIW8gYTD"
-              target="_blank"
-              class="external-link"
-              >YouTube Channel with more information</a
-            >
-          </h3>
-          <div class="embed-container">
-            <iframe
-              src="https://www.youtube.com/embed/XmMq8xEMuLQ"
-              class="embed"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
-            ></iframe>
-          </div>
-        </div>
-        <!-- <div class="expl-container">
-          <div class="margin-bottom text-centered">
-            <a href="https://www.bsc.es/" h-reflang="en" target="_blank" rel="sponsored">
-              <img style="width: 25%;" loading="lazy" alt="BSC" src="assets/img/challenges/logo-bsc.png">
-            </a>
-            <a style="marginLeft: 20px">
-              <img style="width: 12%;" loading="lazy" alt="planTL" src="assets/img/challenges/logo_plantl.png">
-            </a>
-          </div>
-        </div> -->
-      </div>
+      <panel />
       <div class="panel">
         <h1>
           Clinical-microbiological characterisation of SARS-CoV-2 infection in
@@ -264,10 +210,15 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import Vue from 'vue'
+import Panel from '@/components/Panel.vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  components: {
+    Panel,
+  },
+})
 </script>
 
 <style lang="scss" scoped>
