3e8ed9b2874182c7701520e06bb716a81d0b5ca8
Maurici Abad <maurici.abad@glovoapp.com>
2021-05-12 19:57:10 +0200

    Add more info to the challenges

--- src/data/challenges.ts
+++ src/data/challenges.ts
@@ -1,6 +1,8 @@
 export interface Challenge {
   title: string
   company: string
+  videoUrl?: string
+  channel: string
   prize: string
   description: string
 }
@@ -8,15 +10,17 @@ export interface Challenge {
 export const challenges: Challenge[] = [
   {
     company: 'McKinsey & Company',
-    title: 'Hack the crash',
-    description: 'Help us predict damage inflicted in traffic accidents',
+    title: 'FAKE Hack the crash',
+    description: 'FAKE Help us predict damage inflicted in traffic accidents',
     prize: 'Nintendo switch lite',
+    channel: 'mckensey',
   },
   {
     company: 'HP',
-    title: 'Talk to 3D Print!',
+    title: 'FAKE Talk to 3D Print!',
     description:
-      'Are you able to create a 3D geometry that we can print by voice commands? Your creativity is welcomed, unleash your imagination to test your skills! Come to our booth A5 1st floor our through Slack',
+      'FAKE Are you able to create a 3D geometry that we can print by voice commands? Your creativity is welcomed, unleash your imagination to test your skills! Come to our booth A5 1st floor our through Slack',
     prize: '4 Soprocket',
+    channel: 'hp',
   },
 ]
