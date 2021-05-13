3ffb5b7bafcd600fa2c6f546cccfa119f375442f
Maurici Abad <maurici.abad@glovoapp.com>
2021-05-13 13:58:52 +0200

    Add activity images

--- src/data/activities.ts
+++ src/data/activities.ts
@@ -9,8 +9,7 @@ export const activities: Activity[] = [
     title: 'Draw your Biene',
     description:
       'Biene is our mascot and we invite you to create your own personalized Biene. You can draw it by hand, digitally, on a cake... any form of art is welcome 😄\n\n Upload a picture of your Biene on the discord channel or on a social network using the **#drawyourbiene** hashtag. There will be final voting between some of your submissions in order to find a winner.',
-    imageUrl:
-      'https://images-na.ssl-images-amazon.com/images/I/71yqHJPpkBL.png',
+    imageUrl: require('@/assets/img/activities/draw-biene.png'),
   },
   {
     title: 'Photocall',
@@ -21,43 +20,37 @@ export const activities: Activity[] = [
     title: 'HackUphone',
     description:
       "How weird can a sentence become? Let's play a Telephone Game! ☎️\n\n Stay tuned to discord, there will be a message where you can react in order to participate, later on you will receive a picture or a sentence. If you receive a sentence, you then will need to draw it, if you receive a picture you will need to write a sentence describing what the picture is. You will have an hour to do it!",
-    imageUrl:
-      'https://qph.fs.quoracdn.net/main-qimg-fede4f40c289a930f26a505009d647cb',
+    imageUrl: require('@/assets/img/activities/hackuphone.png'),
   },
   {
     title: 'Freehand',
     description:
       "So... let's draw!🎨 We will tell you a topic and you have to draw in 3 minutes something inspired, related to the topic. You can use any medium to draw (by hand, paint, photoshop, illustrator…).\n\n In order to participate in the activity, join the voice channel and later on upload your picture on the text channel, remember that we are not picasso so we will not judge any draw, we only want to have fun.",
-    imageUrl:
-      'https://i1.wp.com/shareicon.net/data/2015/09/15/641226_pencil_512x512.png',
+    imageUrl: require('@/assets/img/activities/freehand.png'),
   },
   {
     title: 'NERF',
     description:
       "Let's chill a little bit playing some Online Nerf rounds! \n\n In HackUPC we always play NERF and in this online edition we will do it in a different way! We will play some 5-minute Krunker rounds. In order to play Krunker you only need to have a browser, no installation is needed. Stay tuned to discord in order to join a round and also enter the voice channel to share the experience with the other players.",
-    imageUrl:
-      'https://scontent-frt3-1.xx.fbcdn.net/v/t1.6435-9/75513512_976132102760848_6887804117331214336_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=cdbe9c&_nc_ohc=RySX4uSmCwAAX8OGPJF&_nc_ht=scontent-frt3-1.xx&oh=a8b530816b590b5414436bafaec227f2&oe=60C17492',
+    imageUrl: require('@/assets/img/activities/nerf.jpg'),
   },
   {
     title: 'SlideShare Show',
     description:
       'The MYTHIC HackUPC activity is back: SLIDE SHARE SHOW!  This time, adapted into an online version. The activity will happen on our twitch channel! The details will be explained during the live… but if you don’t know about this activity long story short: someone will have to present some random slides. The topic of the slides will be chosen by the audience... So we need you to participate in both speaker and audience roles!!',
-    imageUrl:
-      'https://scontent-frt3-2.xx.fbcdn.net/v/t1.6435-9/76654208_976127582761300_6894369524598964224_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=cdbe9c&_nc_ohc=tCTGJ58exHoAX_jGAnZ&_nc_ht=scontent-frt3-2.xx&oh=f0875ecee5c3f74965bbf17b75dba1f4&oe=60C39291',
+    imageUrl: require('@/assets/img/activities/slideshare-show.jpg'),
   },
   {
     title: 'Rubber Duck',
     description:
       'The rubber duck search has begun! 🦆 During the hacking time, every 2 hours, 2 questions will be launched. If you get the answer right and it is one of the first 4 correct answers, then you will get the rubber duck (virtually)!',
-    imageUrl:
-      'https://lilalu-shop.com/media/image/66/85/19/lilalu-quietscheente-gelb-yellow-rubber-duck-F_600x600.webp',
+    imageUrl: require('@/assets/img/activities/rubber-duck.webp'),
   },
   {
     title: 'Yoga',
     description:
       'After the first hacking night, only standing up to go to the bathroom and to grab some snacks... We thought that a good relaxed 30-min yoga session could help you to release some tiredness! Join us in discord and get relaxed! 🧘',
-    imageUrl:
-      'https://freesvg.org/img/Gerald_G_Yoga_Poses_%28stylized%29_1.png',
+    imageUrl: require('@/assets/img/activities/yoga.svg'),
   },
   // {
   //   title: 'FAKE Nerf 2',
