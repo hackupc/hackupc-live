fa5fcb79a235863cec8048242d97d38bf0bf59fb
Carlota Catot <carkbra@gmail.com>
2021-05-14 00:23:14 +0200

    added discord channels

--- src/data/talks.ts
+++ src/data/talks.ts
@@ -13,7 +13,7 @@ export const talks: Talk[] = [
 Why should you consider building a startup? Why is it easier than you think? After university, what paths can you take that nobody is telling you?`,
     speaker: 'HappyScribe',
     time: 'Friday from 10:30 PM to 11:00 PM (UTC+2)',
-    videoUrl: 'https://www.youtube.com/embed/IN48ZgR0nGk',
+    videoUrl: '',
   },
   {
     title: 'Introduction to Typeform Developer Kit',
@@ -21,7 +21,7 @@ Why should you consider building a startup? Why is it easier than you think? Aft
 4 years ago we launched the version of APIs and Webhooks for Typeform, giving developers the tools necessary to build apps and automation around data collection. For HackUPC we are releasing a new tool that will let developers build apps directly inside Typeform UI. This workshop is an introduction to this new framework. No previous experience needed, first-timers welcome.`,
     speaker: 'Typeform',
     time: 'Friday from 11:00 PM to 11:30 PM (UTC+2)',
-    videoUrl: 'https://www.youtube.com/embed/IN48ZgR0nGk',
+    videoUrl: '',
   },
   {
     title: 'A Deep Learning solution to improve match-making in InfoJobs',
