6a2d8497efa4d9786bd824ca7a1acd4e9a8bef34
Maurici Abad <hello@mauriciabad.com>
2022-02-27 16:19:38 +0100

    Add more eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,7 +16,6 @@ module.exports = {
   },
   rules: {
     'prettier/prettier': ['error', require('./prettier.config.js')],
-    'prefer-template': 'warn',
     'vue/block-lang': ['error', { script: { lang: 'ts' } }],
     'vue/component-api-style': ['error', ['script-setup']],
     'vue/component-name-in-template-casing': ['error', 'PascalCase'],
@@ -29,6 +28,9 @@ module.exports = {
     'vue/prefer-true-attribute-shorthand': 'error',
     'vue/v-for-delimiter-style': 'error',
     'vue/v-on-function-call': 'error',
+    'vue/no-undef-components': 'error',
+    'prefer-template': 'warn',
+    'sort-imports': 'warn',
   },
   overrides: [
     {
