6234d77388c680abaa3a55dbf436537e8931d1cc
Maurici Abad <hello@mauriciabad.com>
2022-02-27 21:52:57 +0100

    6

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,7 +21,7 @@ const submitDeadline = computed<string>(() =>
   <div class="container">
     <Panel title="Important information">
       <p>
-        test 5
+        test 6
         <b>
           The projects must be posted on
           <a
