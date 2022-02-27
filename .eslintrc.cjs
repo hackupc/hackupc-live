e2f8cf728e501a1d4e8331f631ecd3eff391be68
Maurici Abad <hello@mauriciabad.com>
2022-02-27 14:09:29 +0100

    Use another eslint file

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+/**
+ * @type {import('eslint").Linter.Config}
+ */
+module.exports = {
+  root: true,
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript/recommended",
+    "@vue/eslint-config-prettier",
+  ],
+  env: {
+    "vue/setup-compiler-macros": true,
+  },
+  rules: {
+    "prefer-template": "warn",
+    "vue/block-lang": ["error", { script: { lang: "ts" } }],
+    "vue/component-api-style": ["error",
+      ["script-setup"]
+    ],
+    "vue/component-name-in-template-casing": ["error", "PascalCase"],
+
+  },
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.spec.{js,ts,jsx,tsx}",
+        "cypress/integration/**.spec.{js,ts,jsx,tsx}",
+      ],
+      extends: ["plugin:cypress/recommended"],
+    },
+    {
+      files: ["*.js", "*.ts"],
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
+      },
+      env: {
+        node: true,
+      },
+    },
+  ],
+  ignorePatterns: ["/dist/"],
+};
