0b32eeec0ced5327e6d3615eabfd207735bd7e90
Maurici Abad <hello@mauriciabad.com>
2022-02-27 14:25:43 +0100

    prettier rule in eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,7 @@ module.exports = {
     "vue/setup-compiler-macros": true,
   },
   rules: {
+    'prettier/prettier': ['error', require('./prettier.config.js')],
     "prefer-template": "warn",
     "vue/block-lang": ["error", { script: { lang: "ts" } }],
     "vue/component-api-style": ["error", ["script-setup"]],
