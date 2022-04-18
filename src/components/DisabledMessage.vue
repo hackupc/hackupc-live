945a6615177d783c8d7582449a9cf52b78afbe23
Maurici Abad <hello@mauriciabad.com>
2022-04-18 15:39:04 +0200

    Add light theme prop

--- src/components/DisabledMessage.vue
+++ src/components/DisabledMessage.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+interface Props {
+  lightTheme?: boolean
+}
+defineProps<Props>()
+</script>
+
 <template>
-  <div class="disabled-msg">
+  <div class="disabled-msg" :class="{ 'disabled-msg__light': lightTheme }">
     Sorry, live is not available yet. <br />
     Come back later.
   </div>
@@ -14,9 +21,15 @@
   align-items: center;
   justify-content: center;
   padding: 1rem;
+  color: #000;
   font-size: 2.5rem;
   font-weight: bolder;
   text-align: center;
   text-shadow: 0 0 1rem rgb(255 255 255);
+
+  &__light {
+    color: #fff;
+    text-shadow: none;
+  }
 }
 </style>
