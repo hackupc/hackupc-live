a4b66824485109de4cbde0ad5d8ef8b2baaac76b
EncryptEx <limusina10@gmail.com>
2022-04-28 19:57:42 +0200

    added newline

--- src/data/missions.ts
+++ src/data/missions.ts
@@ -20,7 +20,9 @@ If you complete each task, you will earn points and when you reach a certain amo
     description: `
 Those who are **face-to-face**, will receive a card with in their swag bag. When you attend to an activity, you should show the card to the organizer in charge and he/she will stamp the card.
 
-If you want to keep track of the number of points earned, you can use [this spreadsheet](https://docs.google.com/spreadsheets/d/16M5zLWoqDwzYZNwg1gOq-D0h3x5SEdXao2RQlP5UMc0/). You should create a copy of it, and then you can check which activities you took part and the tiers where prizes are given.  Once you reach a tier, you can go to the infodesk (same location than baggage) and claim your reward! Don't forget to bring your card to show your attendance. If there isn't an organizer to stamp your card, you should go to the infodesk and show a proof that you have completed the task.`,
+If you want to keep track of the number of points earned, you can use [this spreadsheet](https://docs.google.com/spreadsheets/d/16M5zLWoqDwzYZNwg1gOq-D0h3x5SEdXao2RQlP5UMc0/). You should create a copy of it, and then you can check which activities you took part and the tiers where prizes are given.
+
+Once you reach a tier, you can go to the infodesk (same location than baggage) and claim your reward! Don't forget to bring your card to show your attendance. If there isn't an organizer to stamp your card, you should go to the infodesk and show a proof that you have completed the task.`,
     size: 'small',
   },
   {
