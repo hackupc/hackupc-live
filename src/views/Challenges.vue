5ab1065b563c6bb019803e8f3bf54672c57e2c43
Maurici Abad <hello@mauriciabad.com>
2022-04-29 21:46:37 +0200

    Add hackupc prizes

--- src/views/Challenges.vue
+++ src/views/Challenges.vue
@@ -16,6 +16,26 @@ const hideChallengesAndPrizes = ref(config.hideChallengesAndPrizes)
 <template>
   <div id="challenges">
     <PanelContainer>
+      <Panel title="HackUPC prizes" size="big">
+        <div class="prizes-container">
+          <div class="prize">
+            <h3 class="prize__position">1st</h3>
+            <p class="prize__text">Nintendo Switch Lite</p>
+            <p class="prize__subtext">(For each team member)</p>
+          </div>
+          <div class="prize">
+            <h3 class="prize__position">2nd</h3>
+            <p class="prize__text">Dron IDEA 22</p>
+            <p class="prize__subtext">(For each team member)</p>
+          </div>
+          <div class="prize">
+            <h3 class="prize__position">3rd</h3>
+            <p class="prize__text">Amazfit GTS</p>
+            <p class="prize__subtext">(For each team member)</p>
+          </div>
+        </div>
+      </Panel>
+
       <template v-for="challenge in challenges" :key="challenge.title">
         <Panel v-if="hideChallengesAndPrizes" :title="challenge.company">
           <IconLabel class="channel" centered>
@@ -63,5 +83,35 @@ const hideChallengesAndPrizes = ref(config.hideChallengesAndPrizes)
 
 strong {
   color: $highlight-color;
+}
+
+.prizes-container {
+  display: grid;
+  padding: 16px 0;
+  grid-template-columns: 1fr 1fr 1fr;
+}
+
+.prize {
+  text-align: center;
+
+  &__text {
+    margin: 0 !important;
+    font-size: 1.5em;
+    font-weight: bold;
+  }
+
+  &__subtext {
+    margin: 0 !important;
+    color: $text-soft-color;
+    font-size: 0.9rem;
+    font-weight: normal;
+    opacity: 0.6;
+  }
+
+  &__position {
+    margin: 0 !important;
+    color: #aa8e25;
+    font-size: 2rem;
+  }
 }
 </style>
