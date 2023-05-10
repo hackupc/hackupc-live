89271f3e19f085a942394dbffb28c3c225222c39
carlotacb <carkbra@gmail.com>
2023-05-10 14:01:45 +0200

    faqs

--- src/data/faqs.ts
+++ src/data/faqs.ts
@@ -15,7 +15,7 @@ export const faqs: Faq[] = [
   {
     title: 'Travel reimbursement',
     description: `
-If you have been elected to receive a travel reimbursement, we will contact you after the hackathon to send it to you. You will receive a maximum that has been specified on the email, based on current travelling prices and distances. Requirements to receive travel reimbursement are as follows.
+If you have been elected to receive a travel reimbursement, we will contact you after the hackathon to send it to you. You will receive a maximum that has been specified in the email, based on current travelling prices and distances. Requirements to receive travel reimbursement are as follows.
 - Received an email with travel reimbursement amount.
 - Travel reimbursement receipts submitted before the date specified by HackUPC Team.
 - Project submitted to Devpost before due date.
@@ -27,13 +27,10 @@ The processing of travel reimbursements takes some time. Our schedule depends on
     title: 'How does judging work?',
     description: `
 We will have a judging expo **on ${formatIntervalInTimezone(
-      '31/4/2022 10:15:00',
-      '31/4/2022 13:15:00'
+      '14/5/2023 10:15:00',
+      '14/5/2022 13:15:00'
     )}**.
-
-- If you are hacking in-person, we'll assign you a classroom to present your project (one for HackUPC + as many challenges as you participate), you need to go there so that an organizer gives you a time to present.
-
-- For online hackers, there will be a dedicated google meet room for you to enter and present your project, and we will release information very soon for the online sponsor judging.
+We will assign you a time slot to present your project (one for HackUPC + as many challenges as you participate), you need to go to in front of A3 building and an organizer will assign you to a queue and may give you an estimated time when you will present.
 
 The presentation can only be **3 minutes** long and with **no slides**, Prezi's or any visual support except expect your hack. You can check out other projects at any judging room.
 
@@ -46,10 +43,10 @@ If you participate in a challenge, you'll have to present at the specific sponso
     title: 'When shall I arrive?',
     description: `Registration will start on ${formatDateInTimezone(
       'weekday-time',
-      '29/4/2022 17:00:00'
+      '12/5/2022 16:00:00'
     )}. The opening ceremony is at ${formatDateInTimezone(
       'time',
-      '29/4/2022 19:00:00'
+      '12/5/2022 19:00:00'
     )}.`,
     size: 'small',
   },
@@ -57,13 +54,13 @@ If you participate in a challenge, you'll have to present at the specific sponso
     title: 'When is HackUPC ending?',
     description: `The closing ceremony is expected to end on ${formatDateInTimezone(
       'weekday-time',
-      '31/4/2022 17:00:00'
+      '14/5/2022 15:00:00'
     )}.`,
     size: 'small',
   },
   {
     title: 'What if I cannot make it in time?',
-    description: `No problem. Just come to our [InfoDesk](/map/indoors) for a late check-in. If you are confused, please reach out to any volunteer (white t-shirts) or organizer (pink t-shirts).`,
+    description: `No problem. Just come to our [InfoDesk](/map/indoors) for a late check-in. If you are confused, please reach out to any volunteer (apricot t-shirts) or organizer (red t-shirts).`,
     size: 'small',
   },
   {
@@ -85,5 +82,18 @@ If you participate in a challenge, you'll have to present at the specific sponso
     title: 'Something isn’t working!',
     description: `If something is not working, please come to our [InfoDesk](/map/indoors) and meet our awesome volunteers. They'll love to help you!`,
     size: 'small',
+  },
+  {
+    title: 'Deadline for project submissions?',
+    description: `The deadline for project submissions at ${formatDateInTimezone(
+      'time',
+      '14/5/2022 9:00:00'
+    )}. Submit your projects to [Devpost](https://hackupc-2022.devpost.com/).`,
+    size: 'small',
+  },
+  {
+    title: 'I have another question... Biene?',
+    description: `If you have any other questions, and you can't find it here, make sure to pay us a visit at our [InfoDesk](/map/indoors) or ask at **#help** in Slack.`,
+    size: 'big',
   },
 ]
--- src/views/FAQs.vue
+++ src/views/FAQs.vue
@@ -25,29 +25,5 @@ const submitDeadline = computed<string>(() =>
     >
       <VueMarkdownIt :source="faq.description" />
     </Panel>
-
-    <Panel title="Deadline for project submissions?">
-      The deadline for project submissions is
-      <strong>{{ submitDeadline }}</strong
-      >. Submit your projects to
-      <a
-        target="_blank"
-        href="https://hackupc-2022.devpost.com/"
-        rel="noopener noreferrer"
-        >Devpost</a
-      >.
-    </Panel>
-
-    <Panel title="I have another question... Biene?">
-      If you have any other questions and you can't find it here, make sure to
-      pay us a visit at our
-      <RouterLink
-        :to="{
-          name: 'map',
-          params: { mapId: 'indoors' },
-        }"
-        >InfoDesk</RouterLink
-      >
-    </Panel>
   </PanelContainer>
 </template>
