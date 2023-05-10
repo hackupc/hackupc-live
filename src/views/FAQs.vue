a133bf9dce23bfd4064af2d626496ae5dadef46b
carlotacb <carkbra@gmail.com>
2023-05-10 14:03:07 +0200

    faqs fix

--- src/views/FAQs.vue
+++ src/views/FAQs.vue
@@ -1,18 +1,8 @@
 <script setup lang="ts">
 import Panel from '@/components/Panel.vue'
 import { faqs } from '@/data/faqs'
-import { formatDate } from '@/services/dates'
-import { useScheduleStore } from '@/stores/schedule'
-import { computed } from 'vue'
-import { RouterLink } from 'vue-router'
 import VueMarkdownIt from 'vue3-markdown-it'
 import PanelContainer from '../components/PanelContainer.vue'
-
-const scheduleStore = useScheduleStore()
-
-const submitDeadline = computed<string>(() =>
-  formatDate('weekday-time', scheduleStore.schedule.submitDeadline)
-)
 </script>
 
 <template>
