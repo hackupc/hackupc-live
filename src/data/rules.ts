aae7bf88023882b203ef60ba8857711c377405b9
carlotacb <carkbra@gmail.com>
2023-05-10 13:46:17 +0200

    first rules part

--- src/data/rules.ts
+++ src/data/rules.ts
@@ -18,19 +18,19 @@ Remember that hackathons are like marathons. Some people go to compete but most 
 
 ## The rules of the competition
 1. There is no minimum team size, however, the maximum size is 4 people. There will be 1 prize for each member of the team, independently of the team size.
-1. Teams should be made up exclusively of students (or recent graduates within one year of having graduated) who are not organizers, judges, sponsors, or in any other privileged position at the event. Volunteers are allowed to participate in their free time, accepting the extra difficulty of hacking and volunteering at the same time, but their role won't give them any judging advantage in front of the rest of participants.
-1. All team members should be present at the event. Leaving the venue for some time to hack elsewhere or sleep is fine.
-1. Teams can of course gain advice and support from organizers, volunteers, sponsors, and others.
-1. All work on a project should be done at the hackathon.
-1. Teams can use an idea they had before the event.
-1. Teams can work on ideas that have already been done. Hacks do not have to be “innovative”. If somebody wants to work on a common idea they should be allowed to do so and should be judged on the quality of their hack. These days it’s hard to find something that’s fully original and teams might not know an idea has been done before anyway.
-1. Teams can work on an idea that they have worked on before (as long as they do not re-use code).
-1. Teams can use libraries, frameworks, or open-source code in their projects. Working on a project before the event and open-sourcing it for the sole purpose of using the code during the event is against the spirit of the rules and is not allowed.
-1. Adding new features to existing projects is allowed. Judges will only consider new functionality introduced or new features added during the hackathon in determining the winners.
-1. Teams must stop hacking once the time is up. However, teams are allowed to debug and make small fixes to their programs after time is up. e.g. If during demoing your hack you find a bug that breaks your application and the fix is only a few lines of code, it's okay to fix that. Making large changes or adding new features is not allowed.
-1. Participants who asked for a travel reimbursement need to post a project before the deadline in order to get the reimbursement.
-1. Projects that violate the [Code of Conduct](https://legal.hackersatupc.org/hackupc/code_of_conduct) are not allowed.
-1. Teams can be disqualified from the competition at the organizers' discretion. Reasons might include but are not limited to breaking the Competition Rules, breaking the [Code of Conduct](https://legal.hackersatupc.org/hackupc/code_of_conduct), or other unsporting behaviour.
+2. Teams should be made up exclusively of students (or recent graduates within one year of having graduated) who are not organizers, judges, sponsors, or in any other privileged position at the event. Volunteers are allowed to participate in their free time, accepting the extra difficulty of hacking and volunteering at the same time, but their role won't give them any judging advantage in front of the rest of participants.
+3. All team members should be present at the event. Leaving the venue for some time to hack elsewhere or sleep is fine.
+4. Teams can of course gain advice and support from organizers, volunteers, sponsors, and others.
+5. All work on a project should be done at the hackathon.
+6. Teams can use an idea they had before the event.
+7. Teams can work on ideas that have already been done. Hacks do not have to be “innovative”. If somebody wants to work on a common idea they should be allowed to do so and should be judged on the quality of their hack. These days it’s hard to find something that’s fully original and teams might not know an idea has been done before anyway.
+8. Teams can work on an idea that they have worked on before (as long as they do not re-use code).
+9. Teams can use libraries, frameworks, or open-source code in their projects. Working on a project before the event and open-sourcing it for the sole purpose of using the code during the event is against the spirit of the rules and is not allowed.
+10. Adding new features to existing projects is allowed. Judges will only consider new functionality introduced or new features added during the hackathon in determining the winners.
+11. Teams must stop hacking once the time is up. However, teams are allowed to debug and make small fixes to their programs after time is up. e.g. If during demoing your hack you find a bug that breaks your application and the fix is only a few lines of code, it's okay to fix that. Making large changes or adding new features is not allowed.
+12. Participants who asked for a travel reimbursement need to post a project before the deadline in order to get the reimbursement.
+13. Projects that violate the [Code of Conduct](https://legal.hackersatupc.org/hackupc/code_of_conduct) are not allowed.
+14. Teams can be disqualified from the competition at the organizers' discretion. Reasons might include but are not limited to breaking the Competition Rules, breaking the [Code of Conduct](https://legal.hackersatupc.org/hackupc/code_of_conduct), or other unsporting behaviour.
 
 ## Demos
 After hacking finishes, teams will show their projects to each other and to the judges.
@@ -44,8 +44,7 @@ Teams will be judged on these three criteria. Judges will weigh the criteria equ
 
 - **Technology**: How technically impressive was the hack? Was the technical problem the team tackled difficult? Did it use a particularly clever technique or did it use many different components? Did the technology involved make you go “Wow”?
 - **Idea & Innovation**: How good is the idea?, we want to see things that goes beyond a website that already exists, be creative! There are a lot of things that you can do.
-- **Learning**: Did the team stretch themselves? Did they try to learn something new? What kind of projects have they worked on before? If a team which always does virtual reality projects decides to switch up and try doing a mobile app instead, that exploration should be rewarded.
-We also will be taking in count the design and the usability, so if your application does not look like a 90's website and it's intuitive at using it, extra points for you.
+- **Learning**: Did the team stretch themselves? Did they try to learn something new? What kind of projects have they worked on before? If a team which always does virtual reality projects decides to switch up and try doing a mobile app instead, that exploration should be rewarded. We also will be taking in count the design and the usability, so if your application does not look like a 90's website and it's intuitive at using it, extra points for you.
 
 These criteria will guide judges but ultimately judges are free to make decisions based on their gut feeling of which projects are the most impressive and most deserving.
 
@@ -54,8 +53,7 @@ It's important to note that these judging criteria **do not include**:
 - How good your code is. It doesn't matter if your code is messy, or not well commented, or uses inefficient algorithms. Hacking is about playing around, making mistakes, and learning new things. If your code isn't production ready, we're not going to mark you down.
 - How well you pitch. Hacking is about building and learning, not about selling.
 - How good the idea is. Again, Hackathons aren't about coming up with innovative ideas. It's about building and learning.
-- How well the project solves a problem. You can build something totally useless and as long as you're learning and having fun, that's a good hack! Sometimes a pointless project is one of the best hacks!
-So don't worry about coming up with the next big idea or building the next Facebook. You'll have plenty of time for that outside the hackathon. just focus on learning, having fun, and making new friends. At the end of the day the skills you learn and the friends you make might lead to the next big thing—but you don't have to do that to win a hackathon.
+- How well the project solves a problem. You can build something totally useless and as long as you're learning and having fun, that's a good hack! Sometimes a pointless project is one of the best hacks! So don't worry about coming up with the next big idea or building the next Facebook. You'll have plenty of time for that outside the hackathon. just focus on learning, having fun, and making new friends. At the end of the day the skills you learn and the friends you make might lead to the next big thing—but you don't have to do that to win a hackathon.
 
 ## Remember
 The competition is just a part of the hackathon. To make the most out of the event, try something new, teach other people, and make new friends!
