565a49df48469e28fb2acb32b04414ea5de54160
AdriMM26 <adria.martinez@hackupc.com>
2024-03-24 20:48:19 +0100

    1st iteration Mission

--- src/views/Mission.vue
+++ src/views/Mission.vue
@@ -18,9 +18,7 @@ const hideChallengesAndPrizes = ref(config.hideChallengesAndPrizes)
       :title="mission.title"
       :size="mission.size"
     >
-      <SecretContent v-if="hideChallengesAndPrizes" />
-      <VueMarkdownIt v-else :source="mission.description" />
-      <div
+      <!--<div
         style="
           display: flex;
           width: 100%;
@@ -29,7 +27,9 @@ const hideChallengesAndPrizes = ref(config.hideChallengesAndPrizes)
         "
       >
         <img :src="mission.picture" style="width: 75%" />
-      </div>
+      </div>-->
+      <SecretContent v-if="hideChallengesAndPrizes" />
+      <VueMarkdownIt v-else :source="mission.description" />
     </Panel>
   </PanelContainer>
 </template>
