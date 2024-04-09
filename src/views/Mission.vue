9f67b109e725994e7e152f46d1dd322edd64040f
Adrià Martínez <adria.martinez@build38.com>
2024-04-09 17:06:11 +0200

    Lint

--- src/views/Mission.vue
+++ src/views/Mission.vue
@@ -12,11 +12,13 @@ const hideMission = ref(config.hideMission)
 
 <template>
   <PanelContainer id="missions">
-    <Panel
-      title="The Mission"
-      size="big"
-      >The main goal of <b>HackUPC</b> is not to create a project or win a prize, but to have a great time and create lasting memories. It is for this reason that we have prepared The Mission.
-      To participate, you only need <b>the Mission card</b>, that can be found in your swag bag received at check-in. Complete the tasks and <b>win prizes</b> of all kinds!</Panel>
+    <Panel title="The Mission" size="big"
+      >The main goal of <b>HackUPC</b> is not to create a project or win a
+      prize, but to have a great time and create lasting memories. It is for
+      this reason that we have prepared The Mission. To participate, you only
+      need <b>the Mission card</b>, that can be found in your swag bag received
+      at check-in. Complete the tasks and <b>win prizes</b> of all kinds!</Panel
+    >
     <Panel
       v-for="mission in missions"
       :key="mission.title"
